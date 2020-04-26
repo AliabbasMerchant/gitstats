a3df70a8ef6aec0233bf0b26ded0119f1dc1fb80
Akash raj(CMD) <akashraj9828@gmail.com>
2020-04-27 01:23:12 +0530

    .

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@ rm -rf build/
 npm install
 npm run build
 chmod 600 key.pem
-ssh -o StrictHostKeyChecking=no -p 65002 -i key.pem $USER@$SERVER "rm -rf /domains/gitstats.me/public_html/*;"
-scp -o StrictHostKeyChecking=no -p 65002 -i key.pem -r build/* $USER@$SERVER:/domains/gitstats.me/public_html/
+ssh -o StrictHostKeyChecking=no -i key.pem -p 65002 $USER@$SERVER "rm -rf /domains/gitstats.me/public_html/*;" -p 65002 
+scp -o StrictHostKeyChecking=no -i key.pem -p 65002 -r build/* $USER@$SERVER:/domains/gitstats.me/public_html/ -p 65002 
