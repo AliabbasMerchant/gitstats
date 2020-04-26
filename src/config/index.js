6dca32c70853608ecf957c1a45c6dde203dc7c14
Akash raj(CMD) <akashraj9828@gmail.com>
2020-04-27 04:05:09 +0530

    api test2

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 // const API_BASE_URL_PROD = "https://gitstats-api-prod.herokuapp.com/";
 const API_BASE_URL_DEV = "https://gitstats-api-stage.herokuapp.com/";
-const API_BASE_URL_PROD = "https://api.gitstats.me";
+const API_BASE_URL_PROD = "https://api.gitstats.me/";
 
 // const API_BASE_URL= process.env.NODE_ENV === 'production' ? API_BASE_URL_PROD : API_BASE_URL_DEV;
 const API_BASE_URL=API_BASE_URL_PROD;
