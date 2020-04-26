ca339824d39696e8a1217c4ece3c6bc640b6e530
Akash raj(CMD) <akashraj9828@gmail.com>
2020-04-27 00:59:32 +0530

    ssh port change

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@ rm -rf build/
 npm install
 npm run build
 chmod 600 key.pem
-ssh -o StrictHostKeyChecking=no -i key.pem $USER@$SERVER "rm -rf /domains/gitstats.me/public_html/*;"
-scp -o StrictHostKeyChecking=no -i key.pem -r build/* $USER@$SERVER:/domains/gitstats.me/public_html/
+ssh -o StrictHostKeyChecking=no -p 65002 -i key.pem $USER@$SERVER "rm -rf /domains/gitstats.me/public_html/*;"
+scp -o StrictHostKeyChecking=no -p 65002 -i key.pem -r build/* $USER@$SERVER:/domains/gitstats.me/public_html/
